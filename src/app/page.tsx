aa950cbd8029a5d59f525a3e1a2d8cc595c721f5
Caio Vellani <caiovellani@outlook.com>
2025-08-05 14:46:41 -0300

    feat: add product list and product item components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,46 @@
-import { Header } from "@/components/commom/header";
+import Image from "next/image";
 
-export default function Home() {
+import { Header } from "@/components/commom/header";
+import ProductList from "@/components/commom/product-list";
+import { db } from "@/db";
+
+const Home = async () => {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src="/banner-01.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductList products={products} title="Mais vendidos" />
+
+        <div className="px-5">
+          <Image
+            src="/banner-02.svg"
+            alt="Leve uma vida com estilo"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
-}
+};
+
+export default Home;
---
+++ src/components/commom/product-list.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import ProductItem from "@/components/commom/product-item";
+import type { productTable, productVariantTable } from "@/db/schema";
+
+interface IProductListProps {
+  title: string;
+  products: (typeof productTable.$inferSelect & {
+    variants: (typeof productVariantTable.$inferSelect)[];
+  })[];
+}
+
+const ProductList = ({ title, products }: IProductListProps) => {
+  return (
+    <div className="space-y-6">
+      <h3 className="px-5 font-semibold">{title}</h3>
+      <div className="flex w-full gap-4 overflow-x-auto px-5 [&::-webkit-scrollbar]:hidden">
+        {products.map((product) => (
+          <ProductItem key={product.id} product={product} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ProductList;
