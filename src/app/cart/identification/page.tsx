611862ac90f4906fb5e348f56fcf8a2b89f5d519
Caio Vellani <caiovellani@outlook.com>
2025-08-11 12:23:35 -0300

    feat: add address forms

---
+++ src/app/cart/identification/page.tsx
@@ -0,0 +1,41 @@
+import { eq } from "drizzle-orm";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import Addresses from "@/app/cart/identification/components/addresses";
+import { Header } from "@/components/commom/header";
+import { db } from "@/db";
+import { cartTable } from "@/db/schema";
+import { auth } from "@/lib/auth";
+
+const IdentificationPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session?.user.id) {
+    redirect("/");
+  }
+
+  const cart = await db.query.cartTable.findFirst({
+    where: eq(cartTable.userId, session.user.id),
+    with: {
+      items: true,
+    },
+  });
+
+  if (!cart || cart?.items.length === 0) {
+    redirect("/");
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="px-5">
+        <Addresses />
+      </div>
+    </>
+  );
+};
+
+export default IdentificationPage;
--- src/components/commom/cart.tsx
+++ src/components/commom/cart.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { ShoppingBasketIcon } from "lucide-react";
+import Link from "next/link";
 
 import CartItem from "@/components/commom/cart-item";
 import { Button } from "@/components/ui/button";
@@ -73,7 +74,9 @@ const Cart = () => {
                 <p>{formatCentsToBRL(cart?.totalPriceInCents ?? 0)}</p>
               </div>
 
-              <Button className="mt-5 rounded-full">Finalizar compra</Button>
+              <Button className="mt-5 rounded-full" asChild>
+                <Link href="/cart/identification">Finalizar compra</Link>
+              </Button>
             </div>
           )}
         </div>
